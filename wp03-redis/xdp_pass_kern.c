a8165e01d8162d7c6de3df4a5e771b53bdef4fcb
吴芃 <45660411+PengWu-wp@users.noreply.github.com>
2021-06-11 14:17:46 +0800

    Update xdp_pass_kern.c
    Update tcp&ip checksum. Change payload_len and data to change reply.

--- wp03-redis/xdp_pass_kern.c
+++ wp03-redis/xdp_pass_kern.c
@@ -6,15 +6,33 @@
 #include <linux/udp.h>
 #include <linux/tcp.h>
 #include <linux/string.h>
+#include <linux/types.h>
 #include <bpf/bpf_helpers.h>
 
-struct xrc_tcp_header {
-    __be32 unknown;
-    __be32 tsv;
-    __be32 tsv_r;
-    char data[];
-} __attribute__((__packed__));
-
+#ifndef be16
+#define be16 __be16
+#endif
+#ifndef be32
+#define be32 __be32
+#endif
+#ifndef u32
+#define u32 __u32
+#endif
+#ifndef u16
+#define u16 __u16
+#endif
+#ifndef htonl
+#define htonl __constant_htonl
+#endif
+#ifndef ntohl
+#define ntohl __constant_ntohl
+#endif
+#ifndef htons
+#define htons __constant_htons
+#endif
+#ifndef ntohs
+#define ntohs __constant_ntohs
+#endif
 
 
 #define bpfprint(fmt, ...)                        \
@@ -24,130 +42,165 @@ struct xrc_tcp_header {
                          ##__VA_ARGS__);           \
     })
 
-
-SEC("xdp")
-int bmc_rx_filter_main(struct xdp_md *ctx)
-{
-	void *data_end = (void *)(long)ctx->data_end;
-	void *data = (void *)(long)ctx->data;
-	struct ethhdr *eth = data;
-	struct iphdr *ip = data + sizeof(*eth);
-	void *transp = data + sizeof(*eth) + sizeof(*ip);
-	//struct udphdr *udp;
-	struct tcphdr *tcp;
-	char *payload;
-	__be16 dport;
-
-	struct xrc_tcp_header *xrc_tcp_hdr;
-
-	unsigned char tmp_mac[ETH_ALEN];
-	__be32 tmp_ip;
-	__be16 tmp_port;
-	__be32 tmp_tsv;
-	__be32 tmp_seqnum;
-
-	if (ip + 1 > data_end)
-		return XDP_PASS;
-
-	switch (ip->protocol) {
-		case IPPROTO_TCP:
-			tcp = (struct tcphdr *) transp;
-			if (tcp + 1 > data_end)
-				return XDP_PASS;
-			dport = tcp->dest;
-			payload = transp + sizeof(*tcp);
-			xrc_tcp_hdr = transp + sizeof(*tcp);
-			//bpfprint("Got a TCP pkt. To port: %x\n",dport);
-			//bpfprint("window: %x, check: %x\n",tcp->window,tcp->check);
-			//bpfprint("eth+ip:%d, sizeof(*tcp): %d\n",sizeof(*eth) + sizeof(*ip),sizeof(*tcp));
-			
-			break;
-		default:
-			return XDP_PASS;
-	}
-	if(payload + 23 <= data_end){
-		if ((payload[20] == 'g' || payload[20] == 'G') && (payload[21] == 'e' || payload[21] == 'E') &&
-            (payload[22] == 't' || payload[22] == 'T')){ // This is a GET request
-			//bpfprint("This is a redis get!\n");
-			// //
-			unsigned int mid;
-			memcpy(tmp_mac, eth->h_source, ETH_ALEN);
-			memcpy(eth->h_source, eth->h_dest, ETH_ALEN);
-			memcpy(eth->h_dest, tmp_mac, ETH_ALEN);
-
-			tmp_ip = ip->saddr;
-			ip->saddr = ip->daddr;//0a00_000a	//with 4500 4006 4000
-			ip->daddr = tmp_ip;//0a00_0003
-			ip->tot_len = 0x3d00;//003d
-
-///////////////////////////////////
-			ip->id =  __constant_htons(16218); // just change this!!
-///////////////////////////////////
-
-			mid = __constant_htonl((ip->saddr & 0x0000ffff)<<16) + __constant_htonl((ip->saddr & 0xffff0000)) + __constant_htonl((ip->daddr & 0x0000ffff)<<16) + __constant_htonl((ip->daddr & 0xffff0000)) + __constant_htons(ip->tot_len) + __constant_htons(ip->id) + 50438;	
+struct tcp_header_option { // seems like this is fixed struct?
+    be32 options;
+    be32 tsv;
+    be32 tsv_r;
+} __attribute__((__packed__));
 
 
-			ip->check = ~((__be16)__constant_htonl((mid & 0x0000ffff)<<16) + __constant_htonl((mid & 0xffff0000))); // auto compute!
-			
-			tmp_port = tcp->source;
-			tcp->source = tcp->dest;
-			tcp->dest = tmp_port;
-			//bpfprint("port:%d\n",__constant_htons(tcp->dest));
-			tmp_seqnum = tcp->seq;
-			tcp->seq = tcp->ack_seq; // auto
-			//bpfprint("tmp_seqnum:%x\n",tmp_seqnum);
-			//bpfprint("__constant_htonl:%d\n",__constant_htonl(tmp_seqnum));
-			//bpfprint("__constant_ntohl:%x\n",__constant_ntohl(__constant_htonl(tmp_seqnum) + 21));
-			tcp->ack_seq = __constant_ntohl(__constant_htonl(tmp_seqnum) + 21); // auto
-			tcp->window = 0xfd01;
-			/*bpfprint("tcp->seq_front: %x\n",(tcp->seq & 0xffff0000));
-			bpfprint("tcp->seq_back: %x\n",(tcp->seq & 0x0000ffff)<<16);
-			bpfprint("tcp->ack_seq_front: %x\n",(tcp->ack_seq & 0xffff0000));
-			bpfprint("tcp->ack_seq_back: %x\n",(tcp->ack_seq & 0x0000ffff)<<16);*/
+struct bpf_map_def SEC("maps")
+blacklist_map = {
+        .type        = BPF_MAP_TYPE_ARRAY,
+        .key_size    = sizeof(unsigned int),
+        .value_size  = sizeof(unsigned int),
+        .max_entries = 100,
+};
 
-			tmp_tsv = xrc_tcp_hdr->tsv; // to be optimized.
-			xrc_tcp_hdr->tsv = __constant_ntohl(__constant_htonl(xrc_tcp_hdr->tsv_r) + 2000);
-			xrc_tcp_hdr->tsv_r = tmp_tsv;
-			//bpfprint("tsv_r_front: %x\n",(xrc_tcp_hdr->tsv_r & 0xffff0000));
-			//bpfprint("tsv_r_front: %d\n",__constant_htonl((xrc_tcp_hdr->tsv_r & 0xffff0000))); // done!
-			//bpfprint("tsv_r_back: %x\n",(xrc_tcp_hdr->tsv_r & 0x0000ffff)<<16);
-			//bpfprint("tsv_r_back: %d\n",__constant_htonl((xrc_tcp_hdr->tsv_r & 0x0000ffff)<<16)); // done!
-
-			//bpfprint("tsv_front: %x\n",(xrc_tcp_hdr->tsv & 0xffff0000));
-			//bpfprint("tsv_back: %x\n",(xrc_tcp_hdr->tsv & 0x0000ffff)<<16);
-			mid = 0;
-			mid = __constant_htonl((xrc_tcp_hdr->tsv_r & 0x0000ffff)<<16) + __constant_htonl((xrc_tcp_hdr->tsv_r & 0xffff0000)) + __constant_htonl((xrc_tcp_hdr->tsv & 0x0000ffff)<<16) + __constant_htonl((xrc_tcp_hdr->tsv & 0xffff0000)) + __constant_htonl((tcp->seq & 0x0000ffff)<<16) + __constant_htonl((tcp->seq & 0xffff0000)) + __constant_htonl((tcp->ack_seq & 0x0000ffff)<<16) + __constant_htonl((tcp->ack_seq & 0xffff0000)) + __constant_htons(tcp->dest) + 117760;			
-			//bpfprint("check out mid: %d\n",mid);
-			//bpfprint("check out mid: %x\n",__constant_ntohl(mid));
-			//bpfprint("check out mid_test: %x\n",__constant_htonl((mid & 0x0000ffff)<<16) + __constant_htonl((mid & 0xffff0000)));
-			//bpfprint("check out mid_test: %x\n",~((__be16)__constant_htonl((mid & 0x0000ffff)<<16) + __constant_htonl((mid & 0xffff0000))));
-			
-
-			xrc_tcp_hdr->data[0] = '$';
-			xrc_tcp_hdr->data[1] = '3';
-			xrc_tcp_hdr->data[2] = 0x0d;
-			xrc_tcp_hdr->data[3] = 0x0a;
-			xrc_tcp_hdr->data[4] = 'l';
-			xrc_tcp_hdr->data[5] = 'o';
-			xrc_tcp_hdr->data[6] = 'l';
-			xrc_tcp_hdr->data[7] = 0x0d;
-			xrc_tcp_hdr->data[8] = 0x0a; // static for now...
-			
-			tcp->check = ~((__be16)__constant_htonl((mid & 0x0000ffff)<<16) + __constant_htonl((mid & 0xffff0000))); // auto compute!
-			//bpfprint("check out checksum: %x\n",tcp->check);
+static inline __u16 compute_ip_checksum(struct iphdr *ip) {
+    u32 csum = 0;
+    u16 *next_ip_u16 = (u16 *) ip;
 
 
-			bpf_xdp_adjust_tail(ctx, -12);
-			//bpfprint("pkt TX away.\n\n");
-			return XDP_TX;
-		} else {
-			//bpfprint("Normal TCP pkt...\n");
-			//bpfprint("pkt PASS to kernel.\n\n");
-			return XDP_PASS;
-		}
-	}
-	return XDP_PASS;
+    ip->check = 0;
+
+#pragma clang loop unroll(full)
+    for (int i = 0; i < (sizeof(*ip) >> 1); i++) {
+        csum += *next_ip_u16++;
+    }
+
+    return ~((csum & 0xffff) + (csum >> 16));
 }
 
+static inline __u16
+compute_tcp_checksum(struct iphdr *ip, struct tcphdr *tcp, struct tcp_header_option *tcphdr_ops, char *payload, int offset) {
+    u32 csum = 0;
+    int i;
+    int payload_size;
+    u16 *next_tcp_u16 = (u16 *) &ip->saddr;
 
-char _license[] SEC("license") = "GPL";
+#pragma clang loop unroll(full)
+    for (i = 0; i < 4 + (sizeof(*tcp) >> 1) + (sizeof(*tcphdr_ops) >> 1); i++) {
+        csum += ntohs(*next_tcp_u16);
+        next_tcp_u16 = next_tcp_u16 + 1;
+    }
+    csum += ntohs(0x0600); // TCP protocol 0x06
+    csum += (offset + 32);
+    if (offset % 2 != 0) {
+        payload[offset] = 0x00;
+        payload_size = (offset + 1) >> 1;
+    } else {
+        payload_size = offset >> 1;
+    }
+    next_tcp_u16 = (u16 *) payload;
+
+#pragma clang loop unroll(full)
+    for (i = 0; i < payload_size; i++) {
+        csum += ntohs(*next_tcp_u16);
+        next_tcp_u16 = next_tcp_u16 + 1;
+    }
+
+    return htons(~((csum & 0xffff) + (csum >> 16)));
+}
+
+SEC("xdp")
+int bmc_rx_filter_main(struct xdp_md *ctx) {
+    void *data_end = (void *) (long) ctx->data_end;
+    void *data = (void *) (long) ctx->data;
+    struct ethhdr *eth = data;
+    struct iphdr *ip = data + sizeof(*eth);
+    void *transp = data + sizeof(*eth) + sizeof(*ip);
+    struct tcphdr *tcp;
+    struct tcp_header_option *tcphdr_ops;
+    char *payload;
+    int payload_len = 11;   /// change payload len here!
+    be16 ip_totlen_old;
+    be16 dport;
+
+    unsigned char tmp_mac[ETH_ALEN];
+    be32 tmp_ip;
+    be16 tmp_port;
+    be32 tmp_seqnum;
+
+    be32 tmp_tsv;
+
+    unsigned int key = 0;
+    unsigned int *value;
+    value = bpf_map_lookup_elem(&blacklist_map, &key);
+
+    if (ip + 1 > data_end)
+        return XDP_PASS;
+
+    switch (ip->protocol) {
+        case IPPROTO_TCP:
+            tcp = (struct tcphdr *) transp;
+            if (tcp + 1 > data_end)
+                return XDP_PASS;
+            dport = tcp->dest;
+            if(dport != htons(6379))
+                return XDP_PASS;
+            tcphdr_ops = transp + sizeof(*tcp);
+            payload = transp + sizeof(*tcp) + sizeof(*tcphdr_ops);
+            break;
+        default:
+            return XDP_PASS;
+    }
+    if (payload + 11 <= data_end) {
+        if ((payload[8] == 'g' || payload[8] == 'G') && (payload[9] == 'e' || payload[9] == 'E') &&
+            (payload[10] == 't' || payload[10] == 'T')) { // This is a GET request
+
+            memcpy(tmp_mac, eth->h_source, ETH_ALEN);
+            memcpy(eth->h_source, eth->h_dest, ETH_ALEN);
+            memcpy(eth->h_dest, tmp_mac, ETH_ALEN);
+
+            tmp_ip = ip->saddr;
+            ip->saddr = ip->daddr;
+            ip->daddr = tmp_ip;
+
+            tmp_port = tcp->source;
+            tcp->source = tcp->dest;
+            tcp->dest = tmp_port;
+            ip_totlen_old = ntohs(ip->tot_len);
+            ip->tot_len = htons(52 + payload_len);
+
+            ip->id = htons(16218); // no need to change
+            ip->check = compute_ip_checksum(ip);
+
+            tmp_seqnum = tcp->seq;
+            tcp->seq = tcp->ack_seq; // auto
+            tcp->ack_seq = ntohl(htonl(tmp_seqnum) + ip_totlen_old - 52); // auto
+
+            tcp->window = 0xfd01; // fixed for now...
+
+            tmp_tsv = tcphdr_ops->tsv;
+            tcphdr_ops->tsv = ntohl(htonl(tcphdr_ops->tsv_r) + 200); // Must change, or client will not return the right ACK num.
+            tcphdr_ops->tsv_r = tmp_tsv;
+
+            payload[0] = '$';
+            payload[1] = '5';   /// if you change data, change here!
+            payload[2] = 0x0d;
+            payload[3] = 0x0a;
+            payload[4] = 'h';
+            payload[5] = 'e';
+            payload[6] = 'l';
+            payload[7] = 'l';
+            payload[8] = 'o';   /// if you change data, change here!
+            payload[9] = 0x0d;
+            payload[10] = 0x0a; // static for now...
+            tcp->check = 0;
+            if(payload_len % 2 != 0 && payload + payload_len + 1 <= data_end){
+                tcp->check = compute_tcp_checksum(ip, tcp, tcphdr_ops, payload, payload_len);
+            }else{
+                return XDP_PASS;
+            }
+            bpf_xdp_adjust_tail(ctx, -(ip_totlen_old - 52 - payload_len));
+            return XDP_TX;
+        } else {
+            return XDP_PASS;
+        }
+    }
+    return XDP_PASS;
+}
+
+char _license[]
+SEC("license") = "GPL";
