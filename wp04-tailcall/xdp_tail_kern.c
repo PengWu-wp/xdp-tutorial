4eadc44ff9c7a4d733cae1ef697bd5f4e0d5b02c
吴芃 <523972511@qq.com>
2021-08-12 17:35:58 +0800

    update C

--- wp04-tailcall/xdp_tail_kern.c
+++ wp04-tailcall/xdp_tail_kern.c
@@ -1,1 +1,26 @@
-#include<stdio.h>
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+struct bpf_map_def SEC("maps")
+xdp_progs_map = {
+        .type        = BPF_MAP_TYPE_PROG_ARRAY,
+        .key_size    = sizeof(unsigned int),
+        .value_size  = sizeof(unsigned int),
+        .max_entries = 100,
+};
+
+SEC("xdp")
+int  xdp_main(struct xdp_md *ctx)
+{
+    bpf_tail_call(ctx, &jmp_table, index);
+    return XDP_DROP;
+}
+
+SEC("xdp")
+int  xdp_pass(struct xdp_md *ctx)
+{
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
